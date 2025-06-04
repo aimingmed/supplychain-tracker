e4a3a62ac82d645ca7e490ec01ca5b78793c71bc
leehk <leehongkai@gmail.com>
2025-06-04 11:45:00 +0800

    clean up UserAccount tortoise schema

--- app/backend/models/accounts/tortoise.py
+++ app/backend/models/accounts/tortoise.py
@@ -7,14 +7,6 @@ from enum import Enum
 from pydantic import BaseModel, EmailStr, Field, validator
 from email_validator import validate_email, EmailNotValidError
 
-class TextSummary(models.Model):
-    url = fields.TextField()
-    summary = fields.TextField()
-    created_at = fields.DatetimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.url
-
 class UsersAccount(models.Model):
     username = fields.CharField(max_length=50, pk=True)
     password = fields.CharField(max_length=100)
@@ -24,27 +16,4 @@ class UsersAccount(models.Model):
     def __str__(self):
         return self.username
     
-
-class ProductDetails(models.Model):    
-    productid = fields.CharField(max_length=20, pk=True)
-    productname = fields.CharField(max_length=100)
-    categoryname = fields.CharField(max_length=100)
-    specification = fields.CharField(max_length=20)
-    reorderlevel = fields.IntField()
-    targetstocklevel = fields.IntField()
-    leadtime = fields.IntField()
-
-    def __str__(self):
-        return self.productname
-
-class ProductInventory(ProductDetails):
-    batchid = fields.CharField(max_length=50)
-    quantityinstock = fields.IntField()
-    productiondate = fields.DateField()
-    imageurl = fields.TextField()
-    status = fields.CharField(max_length=20)
-    lastupdated = fields.DatetimeField(auto_now=True)
-    lastupdatedby = fields.CharField(max_length=50)
-
-
-SummarySchema = pydantic_model_creator(TextSummary)
+UsersAccountSchema = pydantic_model_creator(UsersAccount)
